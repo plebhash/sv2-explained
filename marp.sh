bbe9c0ef6f0120cf38e3553e7e014748c2f5f717
plebhash <plebhash@proton.me>
2023-11-16 14:28:13 -0300

    use nix flakes for marp.sh

--- marp.sh
+++ marp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-npx @marp-team/marp-cli@latest md/00-intro.md -o html/00-intro.html --theme-set css/sv2-explained.css
-npx @marp-team/marp-cli@latest md/01-mining.md -o html/01-mining.html --theme-set css/sv2-explained.css
+nix run github:tweag/nix-marp -- md/00-intro.md -o html/00-intro.html --theme-set css/sv2-explained.css 
+nix run github:tweag/nix-marp -- md/01-mining.md -o html/01-mining.html --theme-set css/sv2-explained.css
+
